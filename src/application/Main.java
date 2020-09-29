90520c0be47581e9c0be76560d1d03500ccb37d4
vitorstabile <vitor.stabile@hotmail.com>
2020-09-29 16:22:30 -0300

    Criacao da Classe Embalagem e teste

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.entities.Embalagem;
+import model.entities.MP;
+import model.entities.Produto;
 
 
 public class Main extends Application {
@@ -22,6 +25,40 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		/*launch(args);*/
+		
+		MP mp1 = new MP (89,"800088", "SACO PEBD 1 KG", 0.14);
+
+		MP mp2 = new MP (44,"800043", "ETIQUETA BOPP 100X150 MM", 0.10);
+		
+		MP mp3 = new MP (19,"800018", "CAIXA DE PAPELÃO 5 KG", 1.60);
+		
+		MP mp4 = new MP (35,"800034", "COLA BASTÃO", 17.25);
+
+		Produto produto = new Produto(1, "pao de queijo", 72.0);
+		
+		Embalagem embalagem1 = new Embalagem(1, mp1, produto, 72.0);
+				
+		Embalagem embalagem2 = new Embalagem(2, mp2, produto, 86.0);
+		
+		Embalagem embalagem3 = new Embalagem(3, mp3, produto, 14.0);
+		
+		Embalagem embalagem4 = new Embalagem(4, mp4, produto, 0.1);
+		
+		
+		System.out.println(embalagem1);
+		System.out.println(embalagem2);
+		System.out.println(embalagem3);
+		System.out.println(embalagem4);
+		
+		embalagem1.setQtEmbalagem(100.0);
+		
+		System.out.println(embalagem1);
+		
+		embalagem2.setCustoMP(20.0);
+		
+		System.out.println(embalagem2);
+		
+		System.out.println(embalagem2.getCustoEmbalagem());
 		
 		
 	}
---
+++ src/model/entities/Embalagem.java
@@ -0,0 +1,121 @@
+package model.entities;
+
+import java.io.Serializable;
+
+import model.entities.pk.EmbalagemPK;
+
+public class Embalagem implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private EmbalagemPK id_Produto_MP = new EmbalagemPK();
+	
+	private Integer id_Produto_MP_PK;
+	private Double custoMP;
+	private Double qtEmbalagem;
+	private Double custoEmbalagem;
+	
+	public Embalagem() {
+	}
+
+	public Embalagem(Integer id_Produto_MP_PK, MP mp, Produto produto, Double qtEmbalagem) {
+		this.id_Produto_MP_PK = id_Produto_MP_PK;
+		id_Produto_MP.setMp(mp);
+		id_Produto_MP.setProduto(produto);
+		this.custoMP = mp.getCustoMP();
+		this.qtEmbalagem = qtEmbalagem;
+		this.custoEmbalagem = mp.getCustoMP() * qtEmbalagem;
+	}
+	
+	public MP getMP() {
+		return id_Produto_MP.getMp();
+	}
+
+	public void setMP(MP mp) {
+		id_Produto_MP.setMp(mp);
+	}
+
+	public Produto getProduto() {
+		return id_Produto_MP.getProduto();
+	}
+
+	public void setProduto(Produto produto) {
+		id_Produto_MP.setProduto(produto);
+	}
+
+	public Integer getId_Produto_MP_PK() {
+		return id_Produto_MP_PK;
+	}
+
+	public void setId_Produto_MP_PK(Integer id_Produto_MP_PK) {
+		this.id_Produto_MP_PK = id_Produto_MP_PK;
+	}
+
+	public Double getCustoMP() {
+		return custoMP;
+	}
+
+	public void setCustoMP(Double custoMP) {
+		this.custoMP = custoMP;
+		
+		this.updateCustoEmbalagem();
+	}
+
+	public Double getQtEmbalagem() {
+		return qtEmbalagem;
+	}
+
+	public void setQtEmbalagem(Double qtEmbalagem) {
+		this.qtEmbalagem = qtEmbalagem;
+		
+		this.updateCustoEmbalagem();
+	}
+
+	public Double getCustoEmbalagem() {
+		return custoEmbalagem;
+	}
+
+	public void setCustoEmbalagem(Double custoEmbalagem) {
+		this.custoEmbalagem = custoEmbalagem;
+	}
+	
+	public void updateCustoEmbalagem() {
+		
+		this.custoEmbalagem = this.getQtEmbalagem()*this.custoMP;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id_Produto_MP_PK == null) ? 0 : id_Produto_MP_PK.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Embalagem other = (Embalagem) obj;
+		if (id_Produto_MP_PK == null) {
+			if (other.id_Produto_MP_PK != null)
+				return false;
+		} else if (!id_Produto_MP_PK.equals(other.id_Produto_MP_PK))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Embalagem [id_Produto_MP_PK=" + id_Produto_MP_PK + ", id_MP=" + this.getMP().getIdMP() + ", id_Produto=" + this.getProduto().getIdProduto()
+				+ " custoMP=" + custoMP + ", qtEmbalagem="
+				+ qtEmbalagem + ", custoEmbalagem=" + custoEmbalagem + "]";
+	}
+	
+	
+	
+
+}
