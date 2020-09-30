e39e227f7a9f39abafb63e7318ac345261123a28
vitorstabile <vitor.stabile@hotmail.com>
2020-09-30 12:14:32 -0300

    Criacao da classe auxiliar TestePK

---
+++ src/model/entities/pk/TestePK.java
@@ -0,0 +1,59 @@
+package model.entities.pk;
+
+import java.io.Serializable;
+
+import model.entities.Produto;
+import model.entities.Receita;
+
+public class TestePK implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Receita receita;
+	private Produto produto;
+	
+	public Receita getReceita() {
+		return receita;
+	}
+	public void setReceita(Receita receita) {
+		this.receita = receita;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		result = prime * result + ((receita == null) ? 0 : receita.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TestePK other = (TestePK) obj;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (receita == null) {
+			if (other.receita != null)
+				return false;
+		} else if (!receita.equals(other.receita))
+			return false;
+		return true;
+	}
+	
+	
+
+}
